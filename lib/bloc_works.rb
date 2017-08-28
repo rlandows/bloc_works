5f604a0348bd6b104e6b2b3ac9e207e296ce4a18
rlandows <rlandows@uci.edu>
2017-08-28 00:41:29 -0700

    checkpoint and assignment 4

--- lib/bloc_works.rb
+++ lib/bloc_works.rb
@@ -4,20 +4,41 @@ require_relative "bloc_works/controller"
 require_relative "bloc_works/utility"
 require_relative "bloc_works/router"
 
-
 module BlocWorks
   class Application
     def call(env)
-      puts "This is the env #{env["PATH_INFO"]}"
-      if env["PATH_INFO"]== '/'
-          [200, {'Content-Type' => 'text/html'}, ["Hello Blocheads"]]
-      elsif env["PATH_INFO"]== '/favicon.ico'
-        return fav_icon(env)
-      else
-          puts body = controller_and_action(env)
-          last_one = body[1].to_sym
-          [200, {'Content-Type' => 'text/html'}, [body[0].new(env).send(body[1])]]
-      end
-    end
-  end
-end
+      if env['PATH_INFO'] == '/favicon.ico'
+         return [404, {'Content-Type' => 'text/html'}, []]
+       end
+       puts env
+       rack_app = get_rack_app(env)
+      #  puts rack_app
+       rack_app.call(env)
+     end
+   end
+ end
+
+# module BlocWorks
+#   class Application
+#     def call(env)
+#       puts "This is the env #{env["PATH_INFO"]}"
+#       if env["PATH_INFO"]== '/'
+#           [200, {'Content-Type' => 'text/html'}, ["Hello Blocheads"]]
+#       elsif env["PATH_INFO"]== '/favicon.ico'
+#         return fav_icon(env)
+#       else
+#           body = controller_and_action(env)
+#           last_one = body[1].to_sym
+#           controller = body[0].new(env)
+#           puts "#{body[0]} and this #{body[1]}"
+#           # puts controller.send(body[1])
+#          if puts controller.has_response?
+#          status, header, response = controller.get_response
+#          [status, header, [response.body].flatten]
+#        else
+#           [200, {'Content-Type' => 'text/html'}, controller.send(body[1])]
+#        end
+#       end
+#     end
+#   end
+# end
--- lib/bloc_works/controller.rb
+++ lib/bloc_works/controller.rb
@@ -4,9 +4,66 @@ module BlocWorks
   class Controller
     def initialize(env)
       @env = env
+      @routing_params = {}
     end
 
-    def render(view, locals = {})
+    def dispatch(action, routing_params = {})
+       @routing_params = routing_params
+       text = self.send(action)
+       if has_response?
+         rack_response = get_response
+         [rack_response.status, rack_response.header, [rack_response.body].flatten]
+       else
+         [200, {'Content-Type' => 'text/html'}, [text].flatten]
+       end
+     end
+
+     def self.action(action, response = {})
+       proc { |env| self.new(env).dispatch(action, response) }
+     end
+
+    def request
+       @request ||= Rack::Request.new(@env)
+    end
+
+    def params
+       request.params.merge(@routing_params)
+    end
+
+    def response(text, status = 200, headers = {})
+      raise "Cannot respond multiple times" unless @response.nil?
+      @response = Rack::Response.new([text].flatten, status, headers)
+    end
+
+    def render(*args)
+      if args[0].is_a? Hash
+        controller_action = @routing_params["action"].to_sym
+        args.unshift(controller_action)
+        response(create_reponse_array(*args))
+      else
+       response(create_reponse_array(*args))
+      end
+     end
+
+     def redirect_to(*args)
+      if args[0].is_a? Hash
+        controller_action = @routing_params["action"].to_sym
+        args.unshift(controller_action)
+        response(create_reponse_array(*args))
+      else
+       response(create_reponse_array(*args))
+      end
+     end
+
+     def get_response
+      @response
+    end
+
+    def has_response?
+       !@response.nil?
+     end
+
+    def create_reponse_array(view, locals = {})
        filename = File.join("app", "views", controller_dir, "#{view}.html.erb")
        template = File.read(filename)
        eruby = Erubis::Eruby.new(template)
